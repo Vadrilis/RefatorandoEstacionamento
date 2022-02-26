789219b80a8316aa1aae793de87e0b74e336c925
lAlissial <alissialima@hotmail.com>
2022-02-26 00:47:29 -0300

    ajustes

--- src/main/java/br/edu/ifpb/padroes/estrategy/CalculoDiaria.java
+++ src/main/java/br/edu/ifpb/padroes/estrategy/CalculoDiaria.java
@@ -1,8 +1,13 @@
 package br.edu.ifpb.padroes.estrategy;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.Duration;
 
+@Getter
+@Setter
 public class CalculoDiaria implements CalculoTotal {
     public static BigDecimal VALOR_DIARIA = new BigDecimal("26.00");
 
--- src/main/java/br/edu/ifpb/padroes/estrategy/CalculoMensal.java
+++ src/main/java/br/edu/ifpb/padroes/estrategy/CalculoMensal.java
@@ -1,8 +1,13 @@
 package br.edu.ifpb.padroes.estrategy;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.Duration;
 
+@Getter
+@Setter
 public class CalculoMensal implements CalculoTotal {
     public static BigDecimal VALOR_MENSALIDADE = new BigDecimal("300.00");
 
